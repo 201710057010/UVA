bacbb73f8661e8179f2fd118430d93d0b5987e90
201710057010 <30273619+201710057010@users.noreply.github.com>
2019-02-12 20:55:27 -0500

    Update Solucion.java

--- The 3n+1 problem/Solucion.java
+++ The 3n+1 problem/Solucion.java
@@ -1,9 +1,7 @@
 
 /**
- * Write a description of class Solution here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Eduard Damiam
  */
 import java.util.Scanner;
 public class Solucion
